669ed813d846769bc1e4f6a70d597d77137d531b
rayeeev <rerden01@gmail.com>
2026-03-03 02:17:07 -0800

    Build production.

---
+++ .next/standalone/.next/server/middleware-build-manifest.js
@@ -0,0 +1,21 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/711d597b816a80c1.js",
+    "static/chunks/236f7e5abd6f09ff.js",
+    "static/chunks/30ea11065999f7ac.js",
+    "static/chunks/4dd28bc3f722184a.js",
+    "static/chunks/turbopack-25186fc8e1264445.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: 'standalone',
   compress: true,
   poweredByHeader: false,
   images: {
@@ -8,5 +9,6 @@ const nextConfig: NextConfig = {
     minimumCacheTTL: 60 * 60 * 24 * 30,
   },
 };
+
 
 export default nextConfig;
