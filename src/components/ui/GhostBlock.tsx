05635255a0404077c58e5d0eb5933fee28e53f4e
rayeeev <rerden01@gmail.com>
2026-03-03 02:17:56 -0800

    chore: prepare repository for production push

--- src/components/ui/GhostBlock.tsx
+++
@@ -1,7 +0,0 @@
-interface GhostBlockProps {
-  className?: string;
-}
-
-export default function GhostBlock({ className = "" }: GhostBlockProps) {
-  return <div aria-hidden="true" className={`ghost-skeleton ${className}`} />;
-}
